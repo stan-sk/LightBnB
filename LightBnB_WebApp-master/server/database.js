e755badad9b23d0be888958d0b9cd5360b194ef4
Stanley Lai <stanley.lai@telus.com>
2022-12-14 20:48:55 -0800

    LighthouseBnB Properties

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -147,16 +147,62 @@ exports.getAllReservations = getAllReservations;
 // exports.getAllProperties = getAllProperties;
 
 
-const getAllProperties = (options, limit = 10) => { // limit = 10 is just a  default
-  return pool
-    .query(`SELECT * FROM properties LIMIT $1`, [limit])
-    .then((result) => {
-      // console.log(result.rows);
-      return result.rows;
-    })
-    .catch((err) => {
-      console.log(err.message);
-    });
+const getAllProperties = function (options, limit = 10) {
+  // 1
+  const queryParams = [];
+  // 2
+  let queryString = `
+  SELECT properties.*, avg(property_reviews.rating) as average_rating
+  FROM properties
+  JOIN property_reviews ON properties.id = property_id
+  WHERE TRUE
+  `;
+
+  // 3
+  if (options.city) {
+    queryParams.push(`%${options.city}%`);
+    queryString += ` AND city LIKE $${queryParams.length} `;
+  }
+
+  if (options.minimum_price_per_night) {
+    queryParams.push(options.minimum_price_per_night * 100);
+    queryString += ` AND properties.cost_per_night > $${queryParams.length}`;
+  }
+
+  if (options.maximum_price_per_night) {
+    queryParams.push(options.maximum_price_per_night * 100);
+    queryString += ` AND properties.cost_per_night < $${queryParams.length}`;
+  }
+
+  if (options.owner_id) {
+    queryParams.push(options.owner_id);
+    queryString += ` AND owner_id = $${queryParams.length}`;
+  }
+
+  queryString += ` GROUP BY properties.id`;
+
+
+  if (options.minimum_rating ) {
+    queryParams.push(options.minimum_rating );
+    queryString += ` HAVING AVG(property_reviews.rating) >= $${queryParams.length}`;
+  }
+
+  queryParams.push(limit);
+  queryString += ` ORDER BY cost_per_night LIMIT $${queryParams.length};`;
+
+  // 4
+  // queryParams.push(limit);
+  // queryString += `
+  // GROUP BY properties.id
+  // ORDER BY cost_per_night
+  // LIMIT $${queryParams.length};
+  // `;
+
+  // 5
+  console.log(queryString, queryParams, queryParams.length);
+
+  // 6
+  return pool.query(queryString, queryParams).then((res) => res.rows);
 };
 exports.getAllProperties = getAllProperties;
 
